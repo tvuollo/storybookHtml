67d10affc07ccbc5af765558271f964f41bf1745
Tuomas Vuollo <tuomas.vuollo@tietotalo.fi>
2020-02-07 16:05:57 +0200

    Commit missing file

---
+++ .storybook/copyCodeBlockOverrides.js
@@ -0,0 +1,10 @@
+export const cssOverrides = {
+    cssOverrides: `
+        .container {
+            margin-top: 30px;
+            position: relative;
+        }
+        .displayCode {}
+        .copyButton {}
+    `
+};
